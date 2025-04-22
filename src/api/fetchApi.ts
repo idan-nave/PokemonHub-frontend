411be197d825ee32cdeffd957604da5e09f9fd22
idan <idan.nave.dev@gmail.com>
2025-04-22 10:49:50 +0200

    removed isJson local var from fetchApi

--- src/api/fetchApi.ts
+++ src/api/fetchApi.ts
@@ -1,12 +1,12 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const handleResponse = async (response: Response) => {
-
-  const isJson = response.headers.get("content-type")?.includes("application/json");
   const unknownError = { message: "Unknown error", status: response.status }
 
   if (!response.ok) {
-    const error = isJson ? await response.json() : unknownError;
+    const error = response.headers.get("content-type")?.includes("application/json")
+      ? await response.json()
+      : unknownError;
     throw new Error(error.message || unknownError.message);
   }
 
